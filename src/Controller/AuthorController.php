56ef20e58234a5296b5412fddcbe09255b041585
unknown <jlassi.haroun@esprit.tn>
2023-10-02 10:36:57 +0100

    new

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -8,6 +8,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AuthorController extends AbstractController
 {
+    private  $authors = array(
+        array('id' => 1, 'picture' => '/images/esprit.png',
+            'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ',
+            'nb_books' => 100),
+        array('id' => 2, 'picture' => '/images/william-shakespeare.jpg',
+            'username' => ' William Shakespeare',
+            'email' => ' william.shakespeare@gmail.com', 'nb_books' => 200),
+        array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg',
+            'username' => 'Taha Hussein',
+            'email' => 'taha.hussein@gmail.com', 'nb_books' => 300));
     #[Route('/author', name: 'app_author')]
     public function index(): Response
     {
@@ -15,19 +25,22 @@ class AuthorController extends AbstractController
             'controller_name' => 'AuthorController',
         ]);
     }
-    #[Route('/show/{classe}')]
-    public function show($classe)
-    {
-        // return new Response("Bonjour <i>".$classe."</i>");
-        return $this->render('author/show.html.twig',
-            ['c' => $classe]);
+    // #[Route('/show/{i}/{u}', name: 'show')]
+    // public function show($u,$i)
+    // {
+      
+    //     return $this->render('author/show.html.twig', [
 
-    }
+
+    //         'u' => $u,
+    //         'i'=>$i
+    //     ]);
+    // }
     #[Route('/list')]
     public function list()
     {
         $authors = array(
-            array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg',
+            array('id' => 1, 'picture' => '/images/esprit.png',
                 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ',
                 'nb_books' => 100),
             array('id' => 2, 'picture' => '/images/william-shakespeare.jpg',
@@ -39,11 +52,13 @@ class AuthorController extends AbstractController
         return $this->render('author/list.html.twig',
             ['auth' => $authors]);
     }
-    #[Route('/showAuthor/{i}', name: 'show')]
+    #[Route('/showAuthor/{i}', name: 'showAuth')]
     public function showAuthor($i)
     {
         return $this->render('author/showAuthor.html.twig',
-            ['auth' => $i]);
+            ['id' => $i,
+            'authers'=>$this->authors
+        ]);
 
     }
 
